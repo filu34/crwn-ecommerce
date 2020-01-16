67435a5191eb9ddb64acc8b535219224579bb3bf
Filip Szettel <filu3@protonmail.com>
2020-01-16 02:09:48 +0000

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,10 +36,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     }
   }
 
-  console.log(snapShot);
-  console.log(userAuth);
-  console.log(firestore.doc('users/128fdashadu'));
-
   return userRef;
 }
 
