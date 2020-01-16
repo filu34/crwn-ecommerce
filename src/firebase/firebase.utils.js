bf77e6222a6e71d253c20af3f94bea71aa1cb050
Filip Szettel <filu3@protonmail.com>
2020-01-16 01:55:30 +0000

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,37 @@ const config = {
     messagingSenderId: "740221822080",
     appId: "1:740221822080:web:0b23f12f31e21be73c3265",
     measurementId: "G-NJM16CLLK5"
-  };
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  console.log(snapShot);
+  console.log(userAuth);
+  console.log(firestore.doc('users/128fdashadu'));
+
+  return userRef;
+}
 
 firebase.initializeApp(config);
 
---
+++ src/test.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('OWWa0cisGmIa4DjdpDBM').collection('cartItems').doc("L52INoFSYVjf8Txtzkpa")
+firestore.doc("/users/OWWa0cisGmIa4DjdpDBM/cartItems/L52INoFSYVjf8Txtzkpa")
+firestore.collection("users/OWWa0cisGmIa4DjdpDBM/cartItems")
+
+
+const users = {
+    'OWWa0cisGmIa4DjdpDBM': {
+        'cartItems': {
+            "L52INoFSYVjf8Txtzkpa": {
+                "name": "Blue Hat",
+            },
+            "Un1U2Ih8wzPNGfX04A0O": {
+                "name": "Leather Jacket",
+            }
+        },
+        'displayName': 'Dżon',
+    }
+
+}
